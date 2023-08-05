820e5028a494472a403204c558ec2152a7ab45e9
lynabot <141529796+lynabot@users.noreply.github.com>
2023-08-06 01:26:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+const { Client, GatewayIntentBits, Partials, Collection } = require("discord.js");
+const { Guilds, GuildMembers, GuildMessages } = GatewayIntentBits;
+const { User, Message, GuildMember, ThreadMember } = Partials;
 
+const client = new Client({
+        intents: [Guilds, GuildMembers, GuildMessages],
+        partials: [User, Message, GuildMember, ThreadMember],
+});
+
+const { loadEvents } = require("./Handlers/eventHandler");
+
+client.events = new Collection();
+client.commands = new Collection();
+
+loadEvents(client);
+
+client.login("MTEzNDU3ODkzODYzNTYyMDM2Mg.GQEUCe.4_v3g4OSdfPF974y6MbSgEjNJbapQnbzyeBm54");
